b7a39b8fc862663674fcbbac36f53b09431077aa
lyanjun <limingyang920827>
2017-06-05 15:11:24 +0800

    测试产品列表的数据

---
+++ app/src/main/java/com/sis/clms/constant/ServiceCode.java
@@ -0,0 +1,11 @@
+package com.sis.clms.constant;
+
+/**
+ * 服务编号
+ * Created by DELL on 2017/6/5.
+ */
+
+public interface ServiceCode {
+    //首页
+    String PRODUCT_LIST_CODE = "002090";//产品列表(请求该服务，无需用户登录)
+}
